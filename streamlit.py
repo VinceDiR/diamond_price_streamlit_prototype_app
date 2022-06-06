33bba81c7db008c5c94b42e3cd08ab2680b0c874
Nate <88605064+NateDiR@users.noreply.github.com>
2022-06-06 19:02:07 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -63,4 +63,4 @@ def predict(carat, cut, color, clarity, depth, table, x, y, z):
 
 st.title('Diamond Price Predictor')
 st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
-st.header('Enter the details of the diamond and get the price (in USD):')
+st.header('Enter the characteristics of the diamond:')
