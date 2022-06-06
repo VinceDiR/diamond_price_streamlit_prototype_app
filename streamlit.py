8bb3df568e6b0b9e70640292ffbea24ab520c37c
Nate <88605064+NateDiR@users.noreply.github.com>
2022-06-06 18:03:43 -0400

    update main

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,55 @@ model.load_model('xgb_model.json')
 #Caching the model for faster loading
 @st.cache
 
-def predict(carat, cut, color, clarity, depth, table, price, x, y, z):
+
+
+def predict(carat, cut, color, clarity, depth, table, x, y, z):
+    #Predicting the price of the carat
+    if cut == 'Fair':
+        cut = 0
+    elif cut == 'Good':
+        cut = 1
+    elif cut == 'Very Good':
+        cut = 2
+    elif cut == 'Premium':
+        cut = 3
+    elif cut == 'Ideal':
+        cut = 4
+
+    if color == 'J':
+        color = 0
+    elif color == 'I':
+        color = 1
+    elif color == 'H':
+        color = 2
+    elif color == 'G':
+        color = 3
+    elif color == 'F':
+        color = 4
+    elif color == 'E':
+        color = 5
+    elif color == 'D':
+        color = 6
+    
+    if clarity == 'I1':
+        clarity = 0
+    elif clarity == 'SI2':
+        clarity = 1
+    elif clarity == 'SI1':
+        clarity = 2
+    elif clarity == 'VS2':
+        clarity = 3
+    elif clarity == 'VS1':
+        clarity = 4
+    elif clarity == 'VVS2':
+        clarity = 5
+    elif clarity == 'VVS1':
+        clarity = 6
+    elif clarity == 'IF':
+        clarity = 7
+    
+
+    prediction = model.predict([[carat, cut, color, clarity, depth, table, x, y, z]])
+    return prediction
 
     
